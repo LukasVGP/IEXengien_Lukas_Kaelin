d1b784a2dc121888de7d0b019f7689640f443dc8
LukasVGP <152808843+LukasVGP@users.noreply.github.com>
2025-05-29 20:16:24 -0700

    4

--- VGP242/04_HelloMeshBuilder/WinMain.cpp
+++ VGP242/04_HelloMeshBuilder/WinMain.cpp
@@ -3,28 +3,14 @@
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
-    try
-    {
-        IExeEngine::AppConfig config;
-        config.appName = L"Hello MeshBuilder";
+    IExeEngine::AppConfig config;
+    config.appName = L"Hello MeshBuilder";
 
-        IExeEngine::App& myApp = IExeEngine::MainApp();
+    IExeEngine::App& myApp = IExeEngine::MainApp();
 
-        // Add all states
-        myApp.AddState<ShapeState>("ShapeState");
-        myApp.AddState<CubeState>("Cube");
-        myApp.AddState<PyramidState>("Pyramid");
-        myApp.AddState<RectangleState>("Rectangle");
+    myApp.AddState<ShapeState>("ShapeState");
 
-        // The first added state will be the initial state by default
-        // If you need to specify a different initial state, check if there's a method like:
-        // myApp.SetStartState("ShapeState") or similar in your engine API
+    myApp.Run(config);
 
-        myApp.Run(config);
-    }
-    catch (const std::exception& e)
-    {
-        MessageBoxA(nullptr, e.what(), "Error", MB_OK | MB_ICONERROR);
-    }
     return 0;
 }
